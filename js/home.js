a644207fc04a34fc736b15681e3ccab4f4307230
cad67644 <32313326+cad67644@users.noreply.github.com>
2019-05-06 17:52:27 -0500

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,28 @@
+
+const webInfo = {
+  "webtitle": {
+    "text":"exampleTitle",
+    "css":{
+      
+    }
+  }
+}
+
+/*
+// Web Title
+let webTitle = document.createElement('h1');
+webTitle.textContent = webInfo.webtitle.text;
+webTitle.id = "webTitle";
+$('body').append(webTitle);
+$('#webTitle').css(webInfo.webtitle.css);
+*/
+
+
+
+
+
+
+
+
+
+
